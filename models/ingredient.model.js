b6e657cdd268b45b9ba048415cdc0d27158b2b7b
cwdott <36477596+cwdott@users.noreply.github.com>
2019-12-23 11:57:48 -0500

    Update ingredient.model.js

--- models/ingredient.model.js
+++ models/ingredient.model.js
@@ -3,8 +3,21 @@ const Schema = mongoose.Schema;
 
 let IngredientSchema = new Schema({
   name: { type: String, required: true, max: 100 },
+  servingName: { type: String, required: true, max: 20 },
+  servingAmount: { type: float, required: true },
+  servingUnit: { type: String, required: true, max: 5 },
   calories: { type: Number, required: true },
-  fat: { type: Number, required: true }
+  fat: { type: Number, required: true },
+  fatSaturated: { type: Number, required: true },
+  fatTrans: { type: Number, required: true },
+  cholesterol: { type: Number, required: true },
+  sodium: { type: Number, required: true },
+  carbs: { type: Number, required: true },
+  fiber: { type: Number, required: true },
+  sugar: { type: Number, required: true },
+  sugarIncluded: { type: Number, required: true },
+  sugarAdded: { type: Number, required: true },
+  protein: { type: Number, required: true },
 });
 
 // Export the model
